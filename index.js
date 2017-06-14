e526f297ad87b7898592847eb04522711e4a07db
Michael Tutt <tutt.michael@gmail.com>
2017-06-14 14:15:24 -0700

    board creation

--- index.js
+++ index.js
@@ -6,34 +6,71 @@ var { key } = require('./module.js');
 Promise.promisifyAll(prompt);
 Promise.promisifyAll(fs);
 
-class LiftoffChallengeProgram {
+class Minesweeper {
   constructor() {
-    // this.getNumber();
-    this.logModule();
+    this.bombBoard = null;
+    this.gameBoard = null;
+    this.startGame();
   }
 
-  getNumber() {
-    let numberSchema = [
-      {name: 'number', description: 'Please enter an number', type: 'integer'}
+  startGame() {
+    this.createBoard('bombBoard');
+  }
+
+  nextTurn() {
+
+  }
+
+  printBoard(boardToPrint) {
+    var columns = this[boardToPrint][0].length;
+    this[boardToPrint].forEach((col, ind) => {
+      console.log(`${col.join(' ')}\n`);
+    });
+  }
+
+  createBoard(boardToCreate) {
+    let boardSchema = [
+      {name: 'columns', description: 'Please enter the number of board columns', type: 'integer'},
+      {name: 'rows', description: 'Please enter the number of board rows', type: 'integer'},
+      {name: 'bombs', description: 'Please enter teh number of bombs to place', type: 'integer'}
     ];
 
+    prompt.getAsync(boardSchema)
+    .then(result => {
+      let { rows, columns, bombs } = result;
 
-    prompt.getAsync(numberSchema)
-      .then(result => {
-        let {number} = result;
-        let randomNum = Math.floor(Math.random() * 10);
-        let message = `Your number was: ${number}\nA random number is: ${randomNum}\nThese numbers added together is: ${number + randomNum}`;
-        console.log(message);
-        return this.isNumberEven(number + randomNum);
-      })
-      .then(result => {
-        var message = `The number is ${result ? '' : 'not '}even`;
-        console.log(message);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+      this[boardToCreate] = new Array(rows).fill(0).map(row => new Array(columns).fill('.'));
+      this.printBoard('bombBoard');
+    });
+
   }
+
+  placeBombs() {
+
+  }
+
+  // getNumber() {
+  //   let numberSchema = [
+  //     {name: 'number', description: 'Please enter an number', type: 'integer'}
+  //   ];
+
+
+  //   prompt.getAsync(numberSchema)
+  //     .then(result => {
+  //       let {number} = result;
+  //       let randomNum = Math.floor(Math.random() * 10);
+  //       let message = `Your number was: ${number}\nA random number is: ${randomNum}\nThese numbers added together is: ${number + randomNum}`;
+  //       console.log(message);
+  //       return this.isNumberEven(number + randomNum);
+  //     })
+  //     .then(result => {
+  //       var message = `The number is ${result ? '' : 'not '}even`;
+  //       console.log(message);
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  // }
 
   isNumberEven(number) {
     var result = false;
@@ -51,4 +88,4 @@ class LiftoffChallengeProgram {
 
 }
 
-var program = new LiftoffChallengeProgram();
+var program = new Minesweeper();
